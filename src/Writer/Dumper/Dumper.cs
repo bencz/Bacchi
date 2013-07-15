c4a0e006b8ca82db56e2f8f81a10f96b80f24e3b
Mikael Lyngvig <mikael@lyngvig.org>
2013-07-15 21:16:45 +0200

    Added dummy dumper and README.md file.

---
+++ src/Writer/Dumper/Dumper.cs
@@ -0,0 +1,209 @@
+#region license
+// Copyright (C) 2013 Mikael Lyngvig (mikael@lyngvig.org).  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+// conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice, this list of conditions and the disclaimer below.
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided with the distribution.
+//     * Neither the name of Mikael Lyngvig nor the names of its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+// BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+// SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+/** \file
+ *  Defines the \c Visitor interface, which is used to traverse the Abstract Syntax Tree (AST).
+ */
+
+using Bacchi.Syntax;
+
+namespace Bacchi.Writer
+{
+    /** Dumper used to dump the Abstract Syntax Tree (AST) to a file. */
+    public class Dumper: Visitor
+    {
+        private System.IO.StreamWriter _writer;
+
+        public Dumper(string filename)
+        {
+            _writer = System.IO.File.CreateText(filename);
+        }
+
+        public void Close()
+        {
+            _writer.Close();
+            _writer = null;
+        }
+
+        public object Visit(Argument that)
+        {
+            return null;
+        }
+
+        public object Visit(ArrayExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(ArrayReference that)
+        {
+            return null;
+        }
+
+        public object Visit(ArrayType that)
+        {
+            return null;
+        }
+
+        public object Visit(Assignment that)
+        {
+            return null;
+        }
+
+        public object Visit(BinaryExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(Block that)
+        {
+            return null;
+        }
+
+        public object Visit(BooleanLiteral that)
+        {
+            return null;
+        }
+
+        public object Visit(BooleanType that)
+        {
+            return null;
+        }
+
+        public object Visit(CallStatement that)
+        {
+            return null;
+        }
+
+        public object Visit(Definition that)
+        {
+            return null;
+        }
+
+        public object Visit(File that)
+        {
+            _writer.WriteLine("/* FILE: {0} */", that.Filename);
+            return null;
+        }
+
+        public object Visit(ForallStatement that)
+        {
+            return null;
+        }
+
+        public object Visit(Guard that)
+        {
+            return null;
+        }
+
+        public object Visit(IdentifierExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(IdentifierReference that)
+        {
+            return null;
+        }
+
+        public object Visit(IdentifierType that)
+        {
+            return null;
+        }
+
+        public object Visit(IntegerExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(IntegerLiteral that)
+        {
+            return null;
+        }
+
+        public object Visit(IntegerType that)
+        {
+            return null;
+        }
+
+        public object Visit(MemberExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(Module that)
+        {
+            return null;
+        }
+
+        public object Visit(Parameter that)
+        {
+            return null;
+        }
+
+        public object Visit(ParenthesisExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(Program that)
+        {
+            foreach (File file in that.Files)
+                file.Visit(this);
+            return null;
+        }
+
+        public object Visit(RangeType that)
+        {
+            return null;
+        }
+
+        public object Visit(Statement that)
+        {
+            return null;
+        }
+
+        public object Visit(StringLiteral that)
+        {
+            return null;
+        }
+
+        public object Visit(TupleExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(TupleIndexExpression that)
+        {
+            return null;
+        }
+
+        public object Visit(TupleType that)
+        {
+            return null;
+        }
+
+        public object Visit(UnaryExpression that)
+        {
+            return null;
+        }
+    }
+}
+
