66d9371c43cf37f772ea614b34808dbda9cbcb92
Mikael Lyngvig <mikael@lyngvig.org>
2013-07-20 08:04:23 +0200

    Fixed: Forgot to remove to-do item for last bug fix.

--- src/Tasks.cs
+++ src/Tasks.cs
@@ -1,7 +1,6 @@
 /** \file
  *  Main task list (to-do list).
  *
- *  \todo Figure out where node 98 to 101 went in the dump of the AST.
  *  \todo Delete the \c IntegerExpression class and use \c IntegerLiteral instead.
  *  \todo Add a recursive \c Compare() method that compares two sub-trees recursively.
  */
