50a5981cd5bfc7976f645acbacb4649e54c74430
Mikael Lyngvig <mikael@lyngvig.org>
2013-07-22 05:03:16 +0200

    Fixed: Removed the unused IntegerExpression class.

--- src/Tasks.cs
+++ src/Tasks.cs
@@ -1,7 +1,6 @@
 /** \file
  *  Main task list (to-do list).
  *
- *  \todo Delete the \c IntegerExpression class and use \c IntegerLiteral instead.
  *  \todo Add a recursive \c Compare() method that compares two sub-trees recursively.
  */
 
