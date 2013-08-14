18e40571a830bb319114d3ce45d4850fdc61992f
Mikael Lyngvig <mikael@lyngvig.org>
2013-08-14 03:23:46 +0200

    Added a few items to the master to-do list.

--- src/Tasks.cs
+++ src/Tasks.cs
@@ -1,6 +1,9 @@
 /** \file
  *  Main task list (to-do list).
  *
+ *  \todo Register the GCL standard environment (Boolean, integer, string, false, true) prior to resolving symbols.
+ *  \todo Eliminate the \c Argument class as it is nothing more than an additional layer around \c Expression.
+ *  \todo Ensure that the resolver correctly determines the type of all known symbols - either a type or \c ErrorType.
  *  \todo Drop the idea of using the symbol table for on-going symbol lookup; instead use a reference in each applied occurence.
  *  \todo Fix the problem that partial procedures are not handled at all.
  *  \todo Fix the problem that parameter names are \b not entered into the symbol table.
@@ -8,7 +11,6 @@
  *  \todo Take the address of \c ref arguments when passing them into the called procedure.
  *  \todo If an assignment goes to a \c ref argument, prefix it with asterisk (*).
  *  \todo Clean up partially redundant definitions of \c TupleDefinition and \c TupleType.
- *  \todo Eliminate \c symbols parameter from \c Equal() and rename \c Equal() to \c Compare().
  *  \todo Add a recursive \c Compare() method that compares two sub-trees recursively.
  *  \todo Create a list of all the things I've learned from this project for Alexandre.
  */
